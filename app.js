19197a4c369a51806e67ed1481328f794449fdf9
PedroPMello <pedro.pmello@outlook.com>
2025-05-08 20:32:29 -0300

    Retirada temporária de Avaliação + correções

--- app.js
+++ app.js
@@ -10,13 +10,11 @@ app.use(cors());
 const filmeRoutes = require('./src/routes/filmeRoutes');
 const administradorRoutes = require('./src/routes/administradorRoutes');
 const usuarioRoutes = require('./src/routes/usuarioRoutes');
-const avaliacaoRoutes = require('./src/routes/avaliacaoRoutes');
 const authRoutes = require('./src/routes/authRoutes');
 
 app.use('/api', filmeRoutes);
 app.use('/api', administradorRoutes);
 app.use('/api', usuarioRoutes);
-app.use('/api', avaliacaoRoutes);
 app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 3000;
